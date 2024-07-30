9663518593e58ec9c6c27f66bda38b117906e686
TuPhanLe <tu.phanletuan@gmail.com>
2024-07-30 22:35:07 +0700

    add time calculate

--- src/components/MCQ.tsx
+++ src/components/MCQ.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { Game, Question } from "@prisma/client";
-import { ChevronRight, Timer } from "lucide-react";
+import { differenceInSeconds } from "date-fns";
+import { BarChart, ChevronRight, Timer } from "lucide-react";
 import React from "react";
 import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
-import { Button } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import MCQCounter from "./MCQCounter";
 import { useMutation } from "@tanstack/react-query";
 import { checkAnswerSchema } from "@/schemas/form/quiz";
 import { z } from "zod";
 import axios from "axios";
 import { useToast } from "./ui/use-toast";
-
+import Link from "next/link";
+import { cn, formatTimeDelta } from "@/lib/utils";
 type Props = {
   game: Game & { question: Pick<Question, "id" | "question" | "options">[] };
 };
@@ -20,7 +22,18 @@ const MCQ = ({ game }: Props) => {
   const [selectedChoice, setSelectedChoice] = React.useState<number>(0);
   const [correctAnswers, setCorrectAnswers] = React.useState<number>(0);
   const [wrongAnswers, setWrongAnswers] = React.useState<number>(0);
+  const [hasEnded, setHasEnded] = React.useState<boolean>(false);
+  const [now, setNow] = React.useState<Date>(new Date());
   const { toast } = useToast();
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      if (!hasEnded) {
+        setNow(new Date());
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [hasEnded]);
+
   const currentQuestion = React.useMemo(() => {
     console.log(game.question[questionIndex]);
 
@@ -56,16 +69,60 @@ const MCQ = ({ game }: Props) => {
           });
           setWrongAnswers((prev) => prev + 1);
         }
+        if (questionIndex === game.question.length - 1) {
+          setHasEnded(true);
+          return;
+        }
         setQuestionIndex((prev) => prev + 1);
       },
     });
-  }, [checkAnswer, toast, isChecking]);
+  }, [checkAnswer, toast, isChecking, questionIndex, game.question.length]);
+
+  React.useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key == "1") {
+        setSelectedChoice(0);
+      } else if (event.key === "2") {
+        setSelectedChoice(1);
+      } else if (event.key === "3") {
+        setSelectedChoice(2);
+      } else if (event.key === "4") {
+        setSelectedChoice(3);
+      } else if (event.key === "Enter") {
+        handleNext();
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleNext]);
 
   const options = React.useMemo(() => {
     if (!currentQuestion) return [];
     if (!currentQuestion.options) return [];
     return JSON.parse(currentQuestion.options as string) as string[];
   }, [currentQuestion]);
+
+  if (hasEnded) {
+    return (
+      <div className="absolute flex flex-col justify-center top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 ">
+        <div className="px-4 mt-2 font-semibold text-white bg-green-500 whitespace-nowrap">
+          You completed in{" "}
+          {formatTimeDelta(differenceInSeconds(now, game.timeStarted))}
+        </div>
+        <Link
+          href={`/statistics/${game.id}`}
+          className={cn(buttonVariants(), "mt-2")}
+        >
+          View Statistics
+          <BarChart className="w-4 h-4 ml-2" />
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[80vw] max-w-4xl w-[90vw]">
       <div className="flex flex-row justify-between ">
@@ -79,10 +136,13 @@ const MCQ = ({ game }: Props) => {
           </p>
           <div className="flex self-start mt-3 text-slate-400">
             <Timer className="mr-2" />
-            <span>00:00</span>
+            {formatTimeDelta(differenceInSeconds(now, game.timeStarted))}
           </div>
         </div>
-        <MCQCounter correctAnswers={4} wrongAnswers={5} />
+        <MCQCounter
+          correctAnswers={correctAnswers}
+          wrongAnswers={wrongAnswers}
+        />
       </div>
 
       <Card className="w-full mt-4">
